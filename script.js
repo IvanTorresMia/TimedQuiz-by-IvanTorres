2e2bb1e635f04d9158c41e3de678fce3f8bcb861
Ivan Torres <ivantorresmia27@gmail.com>
2020-09-23 15:42:17 -0700

    code for checking answers and timer

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var button3 = document.getElementById("answer3");
 var button4 = document.getElementById("answer4");
 var endContainer = document.getElementById("end-container");
 var questionsDisplay = document.getElementById("question");
-var timeId = document.getElementById("timerNumber")
+var timeId = document.getElementById("timerNumber");
 var i = 0;
 var secondsLeft = 75;
 var score = 0;
@@ -21,18 +21,18 @@ var quizQuestions = [
       "A drum tunning system",
       "A theoretical system to play notes",
       "A sont by the beattles",
-    ], 
+    ],
     correctAnswer: "A theoretical system to play notes",
   },
   {
     question: ["How many members did the Beattles have"],
     choices: ["3", "6", "John Lennon", "4"],
-    correctAnswer: ["4"],
+    correctAnswer: "4",
   },
   {
     question: ["What instrument was whiplash about?"],
     choices: ["Drums", "Guitar", "Cajon", "steve correl"],
-    correctAnswer: ["Drums"],
+    correctAnswer: "Drums",
   },
   {
     question: ["Who was the most famous drummer in the 60s"],
@@ -42,22 +42,17 @@ var quizQuestions = [
       "Alex Gonzales",
       "Drummer from Paramore",
     ],
-    correctAnswer: ["Buddy Rich"],
+    correctAnswer: "Buddy Rich",
   },
   {
     question: ["Who was queen?"],
-    choices: [
-      "Queen of England",
-      "A band",
-      "A kind of genre",
-      "A bass player",
-    ],
-    correctAnswer: ["A band"],
+    choices: ["Queen of England", "A band", "A kind of genre", "A bass player"],
+    correctAnswer: "A band",
   },
   {
     question: ["Whos the song heroes by?"],
     choices: ["David Bowie", "Paramore", "Bethel", "Metalica"],
-    correctAnswer: ["David Bowie"],
+    correctAnswer: "David Bowie",
   },
   {
     question: ["Who in the beattles was shot outside of a show?"],
@@ -67,7 +62,7 @@ var quizQuestions = [
       "Ivan Torres",
       "John Lennon",
     ],
-    correctAnswer: ["John Lennon"],
+    correctAnswer: "John Lennon",
   },
   {
     question: ["Who wrote the song Reckless love??"],
@@ -77,138 +72,125 @@ var quizQuestions = [
       "Hippo Campus",
       "Gable Price",
     ],
-    correctAnswer: ["Cory Asbury"],
+    correctAnswer: "Cory Asbury",
   },
   {
     question: ["How many strings does a standard guitar have?"],
     choices: ["8", "5", "6", "1"],
-    correctAnswer: ["6"],
+    correctAnswer: "6",
   },
   {
     question: ["Is Music the best thing in the world??"],
     choices: ["Yes", "Heck Yea", "Absolute best", "No I am not a human though"],
-    correctAnswer: ["Absolute best"],
+    correctAnswer: "Absolute best",
   },
 ];
 
-
 //This fuunction will start the quiz
-startBtn.addEventListener("click", function() {
-    startContainer.setAttribute("class", "hide");
-    endContainer.setAttribute("class", "hide");
-    questionsContainer.setAttribute("class", "none")
+startBtn.addEventListener("click", function () {
+  startContainer.setAttribute("class", "hide");
+  endContainer.setAttribute("class", "hide");
+  questionsContainer.setAttribute("class", "none");
 
-    setTime();
-    
-    startQuiz();
-    
-})
-
-
+  setTime();
+  startQuiz();
+});
 
 //This function will update the answers
 function startQuiz() {
- 
-    if (i == quizQuestions.length) {
-        startContainer.setAttribute("class", "hide");
-        endContainer.setAttribute("class", "none");
-        questionsContainer.setAttribute("class", "hide")
-
-        i = 0;
-       
-    }
-    questionsDisplay.textContent = quizQuestions[i]["question"];
-    button1.textContent = quizQuestions[i].choices[0];
-    button2.textContent = quizQuestions[i].choices[1];
-    button3.textContent = quizQuestions[i].choices[2];
-    button4.textContent = quizQuestions[i].choices[3];
-    
-   
+  if (i == quizQuestions.length) {
+    endQuiz();
+  }
+  questionsDisplay.textContent = quizQuestions[i]["question"];
+  button1.textContent = quizQuestions[i].choices[0];
+  button2.textContent = quizQuestions[i].choices[1];
+  button3.textContent = quizQuestions[i].choices[2];
+  button4.textContent = quizQuestions[i].choices[3];
 }
 
-button1.addEventListener("click", function(event) {
-    if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
-        i++;
-        alert("You got it right!");
-        score++
-        startQuiz();
-        
+button1.addEventListener("click", function (event) {
+  if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
+    i++;
+    score++;
+    startQuiz();
+  } else {
+    i++;
+    if (secondsLeft === 0) {
+        endQuiz();
     } else {
-        i++;
-        startQuiz();
+        secondsLeft = secondsLeft - 10;
     }
-})
-button2.addEventListener("click", function(event) {
-    if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
-        i++;
-        alert("You got it right!");
-        score++
-        startQuiz();
+    startQuiz();
+  }
+});
+button2.addEventListener("click", function (event) {
+  if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
+    i++;
+    score++;
+    startQuiz();
+  } else {
+    i++;
+    if (secondsLeft === 0) {
+        endQuiz();
     } else {
-        i++;
-        startQuiz();
+        secondsLeft = secondsLeft - 10;
     }
-})
-button3.addEventListener("click", function(event) {
-    if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
-        i++;
-        alert("You got it right!");
-        score++
-        startQuiz();
+    startQuiz();
+  }
+});
+button3.addEventListener("click", function (event) {
+  if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
+    i++;
+    score++;
+    startQuiz();
+  } else {
+    i++;
+    if (secondsLeft === 0) {
+        endQuiz();
     } else {
-        i++;
-        startQuiz();
+        secondsLeft = secondsLeft - 10;
     }
-})
-button4.addEventListener("click", function(event) {
-    if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
-        i++;
-        alert("You got it right!");
-        score++
-        startQuiz();
+    startQuiz();
+  }
+});
+button4.addEventListener("click", function (event) {
+  if (event.target.innerHTML === quizQuestions[i]["correctAnswer"]) {
+    i++;
+    score++;
+    startQuiz();
+  } else {
+    i++;
+    if (secondsLeft === 0) {
+        endQuiz();
     } else {
-        i++;
-        startQuiz();
+        secondsLeft = secondsLeft - 10;
     }
-    
-})
+    startQuiz();
+  }
+});
+
+// This function ends the quiz
+function endQuiz() {
+  startContainer.setAttribute("class", "hide");
+  endContainer.setAttribute("class", "none");
+  questionsContainer.setAttribute("class", "hide");
+  i = 0;
+  clearInterval(timerInterval);
+  timeId.textContent = 0;
+}
 
 
-
-
-
-// var buttons = document.querySelectorAll('.buttonHook').forEach(item => {
-//     item.addEventListener('click', function(event) {
-//      if (event.target.innerHTML = quizQuestions[i].correctAnswer[i]) {
-//        i++
-//         startQuiz()
-//      }
-    
-        
-//     });
-//   })
-
-//This Function will check the answers
-
-
-   
+// This Function will start the tiimer and end the quiz if it reaches 0
 function setTime() {
-  timerInterval = setInterval(function() {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timeId.textContent = secondsLeft;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      timeId.textContent = 0;
+    if (secondsLeft === 0) {
+      endQuiz();
     }
-
   }, 1000);
 }
-
-
-
-
-
 
 // // local storage is module 4 activity 25-28
 // var score = 2;
